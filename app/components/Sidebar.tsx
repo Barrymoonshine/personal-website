7769e60f76c66f08fb53da08bc130611c4862422
Barrymoonshine <nickshwrd@gmail.com>
2025-11-23 12:26:41 +0000

    Add sidebar icons and links

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Sidebar() {
     return (
         <aside id="sidebar">
-            <section id="widget_1">NS</section>
-            <section id="widget_2">LinkedIn</section>
-            <section id="widget_3">GitHub</section>
+            <section>NS</section>
+            <section><a href="https://www.linkedin.com/in/nick-sheward-5a990977/"><img src="/linkedin.png" alt="LinkedIn icon" className="icons"/></a></section>
+            <section><a href="https://github.com/Barrymoonshine"><img src="/github.png" alt="GitHub icon" className="icons"/></a></section>
         </aside>
     );
 }
