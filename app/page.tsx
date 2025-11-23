2508eac318a4c9f794fc6fbc847177d1f7ce0cc8
Barrymoonshine <nickshwrd@gmail.com>
2025-11-23 20:27:51 +0000

    Add content and styling to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import styles from './homePage.module.css';
+import Link from "next/link";
 
 export default function HomePage() {
     return (
         <div className={styles.homePageContainer}>
-            <div>
-                <h1>Hi I'm Nick Sheward, welcome to my personal website!</h1>
+            <div className={styles.content}>
+                <h1 className={styles.heroHeading}>Hey, I’m Nick!</h1>
+
+                <p className={styles.lead}>
+                    I'm a self taught Software Engineer based in Bristol UK and a former Product Manager. Some of my skills include:
+                </p>
+
+                <ul className={styles.skillsList}>
+                    <li>Experienced across the full stack, backend leaning</li>
+                    <li>AWS &amp; MongoDB qualified</li>
+                    <li>Former Product Manager — with a strong customer a business focus</li>
+                </ul>
+
+                <Link href="/about" className={styles.ctaButton}><img src="/list.png" alt="About me icon" className={styles.headerIcons}/>About Me</Link>
             </div>
             <div>
                 <img src="/ghibli-me.png" alt="AI generated image of me in Studio Ghibli style" className={styles.homePageImage}/>
