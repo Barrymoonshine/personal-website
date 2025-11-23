178b21a7abdf2b6f15eb55e6f50c06e4556bc697
Barrymoonshine <nickshwrd@gmail.com>
2025-11-23 15:26:59 +0000

    Add home page image and further styling of modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import styles from './homePage.module.css';
+
 export default function HomePage() {
     return (
-        <div>
-            <h1>Hi I'm Nick Sheward, welcome to my personal website!</h1>
+        <div className={styles.homePageContainer}>
+            <div>
+                <h1>Hi I'm Nick Sheward, welcome to my personal website!</h1>
+            </div>
+            <div>
+                <img src="/ghibli-me.png" alt="AI generated image of me in Studio Ghibli style" className={styles.homePageImage}/>
+            </div>
         </div>
     );
 }
