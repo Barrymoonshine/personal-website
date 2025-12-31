65f2273123e3eb1775d93b21eab909c445a23f7c
Barrymoonshine <nickshwrd@gmail.com>
2025-12-31 12:56:56 +0000

    Add CV CTA button to about page

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/dist/client/link';
 import styles from './About.module.css';
 
 export default function AboutPage() {
@@ -14,7 +15,14 @@ export default function AboutPage() {
                         </p>
                         <p>
                             Outside of work, I like to go to the gym, play the piano, collect vinyl records, and spend time with my family — including my cat, Mr Sprinkles.
-                        </p>       
+                        </p>
+                         <p>
+                            I'm always keen to contribute to projects which I think are important and find interesting, and happy to chat with others with similar interests. Please get in touch if you'd like more information about my experiences to date.
+                        </p>
+                        <Link href="/cv" className={styles.ctaButton}>
+                            <img src="/documents.png" alt="Documents icon" className={styles.headerIcons}/>
+                            Request my CV
+                        </Link>       
                     </div>
                     <div>
                         <img src="/me.jpeg" alt="Picture of me" className={styles.meImg}></img>
